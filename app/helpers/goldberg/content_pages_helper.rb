f51d7be8c773e612942f3ac8f59aba476ef104a8
Erhard Karger <erhard@kargers.org>
2009-05-15 21:13:09 +0200

    Bugfix commtent only possible after login fails when user=nil

--- app/helpers/goldberg/content_pages_helper.rb
+++ app/helpers/goldberg/content_pages_helper.rb
@@ -25,8 +25,10 @@ END
       #@comments = Comment.find(:all)
       @comments = Comment.find(:all, :conditions => ["content_page_id =? AND kind = ?", @content_page.id,"content"],:order => ["created_at desc"])  
       @comment = Goldberg::Comment.new
-      @comment.name  = Goldberg::user.fullname
+      if Goldberg::user != nil then 
+      @comment.name  = Goldberg::user.fullname 
       @comment.email = Goldberg::user.email 
+      end
       @comment.content_page_id = @content_page.id
       render :partial => "goldberg/comments/displayComments"    
     end
